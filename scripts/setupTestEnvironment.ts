74ced10f238f2c3e410bda7fbf0e7cf1bd665b42
Thomas Northall-Little <tomnlittle@outlook.com>
2019-02-05 16:36:14 +1100

    slack environment setup script

---
+++ scripts/setupTestEnvironment.ts
@@ -0,0 +1,77 @@
+/* tslint:disable:no-console ter-max-len */
+import { WebClient } from '@slack/client';
+import { createReadStream, writeFileSync } from 'fs';
+import * as _ from 'lodash';
+
+const OUTPUT_FILE = '.env';
+
+if (process.argv.length !== 3) {
+  console.log('Supply your slack token');
+  console.log('');
+  console.log('yarn setup:test:environment token=YOUR_TOKEN_HERE');
+  process.exit(1);
+}
+
+const token = process.argv[2].replace('token=', '');
+
+const client = new WebClient(token);
+
+// create public and private channel
+async function createChannels () {
+
+  const validChannelName = (prefix = '') => `${prefix}-${new Date().getDay()}-${new Date().getMonth()}-${new Date().getFullYear()}_${new Date().getMilliseconds()}`;
+
+  // Create the a public channel
+  const channelName = validChannelName('PUB_TEST');
+  const channel = await client.channels.create({ name: channelName });
+
+  // Create a private group
+  const groupName = validChannelName('PRIV_TEST');
+  const group = await client.groups.create({ name: groupName });
+
+  return {
+    channel,
+    group,
+  };
+}
+
+// upload a basic file
+async function uploadFile (channel: string) {
+  const filename = `${channel}'s Root Test File ${new Date()}.png`;
+
+  const fileInfo = client.files.upload({
+    channels: channel,
+    file: createReadStream(__dirname + '/sample_image.png'),
+    filename,
+    initial_comment: 'Root File for testing purposes',
+  });
+
+  return fileInfo;
+}
+
+createChannels().then(async ({ channel, group }) => {
+
+  const channelId = _.get(channel, 'channel.id');
+  const groupId = _.get(group, 'group.id');
+
+  const publicChannelFile = await uploadFile(channelId);
+  const privateChannelFile = await uploadFile(groupId);
+
+  const data = `
+  SLACK_BOT_ID=
+  SLACK_BOT_TOKEN=
+
+  SLACK_USER_ID=
+  SLACK_USER_TOKEN=${token}
+
+  SLACK_PRIVATE_CHANNEL=${groupId}
+  SLACK_PRIVATE_CHANNEL_FILE_ID=${_.get(privateChannelFile, 'file.id')}
+
+  SLACK_PUBLIC_CHANNEL=${channelId}
+  SLACK_PUBLIC_CHANNEL_FILE_ID=${_.get(publicChannelFile, 'file.id')}
+  `;
+
+  writeFileSync(__dirname + `/${OUTPUT_FILE}`, data, 'utf8');
+
+  console.log('Completed');
+});
