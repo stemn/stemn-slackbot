85c97776fbc33847cef8c62a2f75d7e53d260775
Thomas Northall-Little <tomnlittle@outlook.com>
2019-02-07 14:20:04 +1100

    server up message

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
+/* tslint:disable:no-console */
 import { STEMN_SLACK_BOT_SERVER_PORT } from './config/app';
 import { server } from './server';
 
-server.listen(STEMN_SLACK_BOT_SERVER_PORT);
+server.listen(STEMN_SLACK_BOT_SERVER_PORT, () => console.log(`Listening on ${STEMN_SLACK_BOT_SERVER_PORT}`));
