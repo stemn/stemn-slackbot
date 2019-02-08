9796bcd7d38d118e3e981e45248f3376266bab2b
Thomas Northall-Little <tomnlittle@outlook.com>
2019-02-08 12:01:10 +1100

    removed redundant code

--- src/client/channel/getAll/getAll.ts
+++
@@ -1,8 +0,0 @@
-import { Client } from '../../client';
-import { IClientChannelInfo } from './IClientChannelInfo';
-
-export async function getAllChannels ({ client }: {
-  client: Client;
-}): Promise<IClientChannelInfo> {
-  return client.channels.list() as Promise<IClientChannelInfo>;
-}
--- src/client/index.ts
+++ src/client/index.ts
@@ -1,4 +1,3 @@
-export * from './channel';
 export * from './file';
 export * from './chat';
 export * from './client';
