c6ee100c550832babb9bfc046a3917e124a5966f
Thomas Northall-Little <tomnlittle@outlook.com>
2019-02-08 15:37:32 +1100

    converted bash script to shell script

--- scripts/generateSchemas.sh
+++ scripts/generateSchemas.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 SCHEMAS_FOLDER=schemas
 INDEX_FILE="${SCHEMAS_FOLDER}/index.ts"
 
@@ -5,21 +6,25 @@ mkdir -p $SCHEMAS_FOLDER
 
 rm $INDEX_FILE
 
-INTERFACES=(
+INTERFACES='
   ./src/webhooks/install/IWebhookInstall.ts
-)
+'
 
-for i in ${INTERFACES[@]}; do
-  FILENAME="$(basename $i | cut -d'.' -f1)"
+for i in $INTERFACES; do
+  FILENAME="$(basename "$i" | cut -d'.' -f1)"
   yarn quicktype \
-    --src $i \
+    --src "$i" \
     -l schema \
     -o "$SCHEMAS_FOLDER/${FILENAME}.json"
 
   FILENAME_SCHEMA="${FILENAME}Schema"
 
-  echo "import * as ${FILENAME_SCHEMA} from './${FILENAME}.json';" >> $INDEX_FILE
-  echo "const ${FILENAME} = ${FILENAME_SCHEMA}.definitions.${FILENAME};" >> $INDEX_FILE
-  echo "export { ${FILENAME} };" >> $INDEX_FILE
-  echo >> $INDEX_FILE
+  {
+          echo "import * as ${FILENAME_SCHEMA} from './${FILENAME}.json';"
+          echo "const ${FILENAME} = ${FILENAME_SCHEMA}.definitions.${FILENAME};"
+          echo "export { ${FILENAME} };"
+          echo
+
+  } >> $INDEX_FILE
+
 done
